7d86d576aca60db0aaef7517f3f126f67332fb8e
kalisthenes <kalistheneyes@gmail.com>
2025-09-23 15:49:56 +0500

    add new materials

---
+++ .scripts/update-readme.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+git add README.md README.ru.md
+git commit -m "update README.md files"
+git push origin
+
---
+++ .scripts/update.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+git add .
+git commit -m "add new materials"
+git push origin
+
